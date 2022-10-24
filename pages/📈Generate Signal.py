dd07b369c88a22c371f3152d39f72d8ab8cd14e5
Your Name <marinanasser8@gmail.com>
2022-10-24 12:06:57 +0200

    columns

--- pages/📈Generate Signal.py
+++ pages/📈Generate Signal.py
@@ -1,38 +1,19 @@
+from ast import With
 import streamlit as st  # data web app development
 import matplotlib.pyplot as plt
 import numpy as np  # np mean, np random ,np asarray, np 
 import pandas as pd
 
+st.set_page_config(
+    page_title="DSP TASK 1",
+    page_icon="✅",
+    layout = "wide",
+    initial_sidebar_state="expanded"
+)
+
+c1, c2 = st.columns((5, 1))
 
 st.title('Customise your signal')
-
-uploaded_file = st.file_uploader("Choose a CSV file 📂 ")
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    
-    list_of_columns=df.columns
-
-    fig, ax = plt.subplots()
-    df = df.drop_duplicates(keep = 'first',subset=[list_of_columns[0]])
-    analogSignalTime = df[list_of_columns[0]].to_numpy()
-    analogSignalValue = df[list_of_columns[1]].to_numpy()
-    ax.plot(analogSignalTime,analogSignalValue)
-    # st.write(type(analogSignalTime))
-    # st.write(type(analogSignalValue))
-    
-    if 'primaryKey' not in st.session_state:
-        st.session_state['primaryKey'] = 0
-    if 'signal' not in st.session_state:
-        st.session_state['signal'] = {}
-    if 'uploaded' not in st.session_state:
-        st.session_state['uploaded'] = {}
-    
-    st.session_state['primaryKey'] = st.session_state['primaryKey'] + 1
-    st.session_state['signal'][st.session_state['primaryKey']] = [analogSignalTime,analogSignalValue]
-    st.session_state['uploaded'][st.session_state['primaryKey']] = True
-    st.plotly_chart(fig,use_container_width=True)
-
 
 
 if 'noise' not in st.session_state:
@@ -49,6 +30,39 @@ if 'signal' not in st.session_state:
 
 if 'uploaded' not in st.session_state:
     st.session_state['uploaded'] = {}
+
+
+
+with c1:
+    uploaded_file = st.file_uploader("Choose a CSV file 📂 ")
+
+
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        
+        list_of_columns=df.columns
+
+        fig, ax = plt.subplots()
+        df = df.drop_duplicates(keep = 'first',subset=[list_of_columns[0]])
+        analogSignalTime = df[list_of_columns[0]].to_numpy()
+        analogSignalValue = df[list_of_columns[1]].to_numpy()
+        ax.plot(analogSignalTime,analogSignalValue)
+        # st.write(type(analogSignalTime))
+        # st.write(type(analogSignalValue))
+        
+        if 'primaryKey' not in st.session_state:
+            st.session_state['primaryKey'] = 0
+        if 'signal' not in st.session_state:
+            st.session_state['signal'] = {}
+        if 'uploaded' not in st.session_state:
+            st.session_state['uploaded'] = {}
+        
+        st.session_state['primaryKey'] = st.session_state['primaryKey'] + 1
+        st.session_state['signal'][st.session_state['primaryKey']] = [analogSignalTime,analogSignalValue]
+        st.session_state['uploaded'][st.session_state['primaryKey']] = True
+        st.plotly_chart(fig,use_container_width=True)
+
+
 
 
 #sliders
@@ -113,7 +127,6 @@ for index, value in st.session_state['checkBoxes'].items():
         leftCheckBoxes.append(index)
 
 
-st.sidebar.text('check the signal first')
 
 
 # showing the signal according to the changes of the sidebar sliders
